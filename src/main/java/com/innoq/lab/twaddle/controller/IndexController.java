312be97ecbedd7666db1880bde4328d0dc023c30
Christian Oberschulte <christian.oberschulte@innoq.com>
2018-01-25 14:27:05 +0100

    user form

--- src/main/java/com/innoq/lab/twaddle/controller/IndexController.java
+++ src/main/java/com/innoq/lab/twaddle/controller/IndexController.java
@@ -3,15 +3,53 @@ package com.innoq.lab.twaddle.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.fromMappingName;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/twaddle")
+    @GetMapping(name = "index", value = "/twaddle")
     public String index(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
     }
 
+    @GetMapping("/twaddle/user/add")
+    public String addUser() {
+        return "user";
+    }
+
+    @PostMapping("/twaddle/user/create")
+    public ModelAndView createUser(@ModelAttribute UserForm userForm) {
+        System.out.println("Username: " + userForm.getUserName());
+        System.out.println("Passwd:   " + userForm.getPasswd());
+        return new ModelAndView(new RedirectView(fromMappingName("index").arg(0, userForm.getUserName()).build()));
+    }
+
+    public static class UserForm {
+        private String userName;
+        private String passwd;
+
+        public String getPasswd() {
+            return passwd;
+        }
+
+        public void setPasswd(String passwd) {
+            this.passwd = passwd;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+    }
 }
